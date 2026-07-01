6b7a4e079a499bf4fffde822de49993155edb7fb
Vuthivath <vathvath786@gmail.com>
2026-07-01 09:05:17 +0700

    change pagination

--- backend/book/views.py
+++ backend/book/views.py
@@ -21,7 +21,7 @@ User = get_user_model()
 # ── Pagination ─────────────────────────────────────────────────────────────────
 
 class BookPagination(PageNumberPagination):
-    page_size = 15
+    page_size = 10
     page_size_query_param = 'page_size'
     max_page_size = 100
 
